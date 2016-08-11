b45d3a3264800c9abae74e51cac7b4a098495fcf
ciaua <ciauaishere@gmail.com>
2016-08-11 19:34:00 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.doctest',
-    'sphinx.ext.linkcode',
+    # 'sphinx.ext.linkcode',
     'sphinx.ext.autosummary',
     'numpydoc'
 ]
