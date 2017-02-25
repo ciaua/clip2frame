99fa6f4f4d3d17caac4be27c1a5581b8c1f4b927
Ciaua <ciauaishere@gmail.com>
2017-02-25 04:02:10 -0500

    update train_model.py

--- Scripts/network_structure.py
+++ Scripts/network_structure.py
@@ -20,6 +20,13 @@ def build_fcn_gaussian_multiscale(mean_std_list):
         network = lasagne.layers.InputLayer(
             shape=(None, 1, None, 128), input_var=input_var)
 
+        # standardization
+        network = layers.standardize(
+            network,
+            mean, std,
+            shared_axes=[0, 1, 2]
+        )
+
         # early conv layers
         earlyconv_list = list()
 
@@ -107,6 +114,13 @@ def build_fcn_fixedgaussian_multiscale(mean_std_list):
         network = lasagne.layers.InputLayer(
             shape=(None, 1, None, 128), input_var=input_var)
 
+        # standardization
+        network = layers.standardize(
+            network,
+            mean, std,
+            shared_axes=[0, 1, 2]
+        )
+
         # early conv layers
         earlyconv_list = list()
 
@@ -180,78 +194,85 @@ def build_fcn_fixedgaussian_multiscale(mean_std_list):
     return network, input_var_list
 
 
-def fcn_multiscale(
-        input_var_list,
-        early_conv_dict_list,
-        late_conv_dict,
-        dense_filter_size,
-        final_pool_function=T.max,
-        input_size_list=[128], output_size=188,
-        p_dropout=0.5
-        ):
-    '''
-    early_conv_dict_list: list
-        each element in the list is a dictionary containing the following keys:
-        'conv_filter_list', 'pool_filter_list', 'pool_stride_list'
+def build_fcn_multiscale(mean_std_list):
 
-    late_conv_dict: dict
-        it contains the following keys:
-        'conv_filter_list', 'pool_filter_list', 'pool_stride_list'
+    input_var_list = list()
 
-    dense_filter_size: int
-        the filter size of the final dense-like conv layer
+    # A stack of convolution layers for each scale
+    for ii, [mean, std] in enumerate(mean_std_list):
+        # input tensor
+        input_var = T.tensor4('input.{}'.format(ii))
+        input_var_list.append(input_var)
 
-    pool_filter_list: list
-        each element is an integer
+        # input layer
+        network = lasagne.layers.InputLayer(
+            shape=(None, 1, None, 128), input_var=input_var)
 
-    pool_stride_list: list
-        each element is int or None
+        # standardization
+        network = layers.standardize(
+            network,
+            mean, std,
+            shared_axes=[0, 1, 2]
+        )
 
-    '''
-    assert(len(early_conv_dict_list) == len(input_var_list) ==
-           len(input_size_list))
+        # early conv layers
+        earlyconv_list = list()
 
-    # early conv layers
-    conv_network_list = list()
-    total_stride_list = list()
-    for jj, [early_conv_dict, input_var, input_size] in enumerate(zip(
-            early_conv_dict_list, input_var_list, input_size_list)):
-        input_network = lasagne.layers.InputLayer(
-            shape=(None, 1, None, input_size), input_var=input_var)
+        network = cl.Conv2DXLayer(
+            network,
+            num_filters=32, filter_size=(8, 128),
+            stride=(1, 1),
+            nonlinearity=lasagne.nonlinearities.rectify,
+            name='early_conv.{}_1'.format(ii)
+        )
+        network = layers.MaxPool2DXLayer(
+            network,
+            pool_size=(4, 1),
+            stride=(4, 1),
+            ignore_border=False,
+            name='early_maxpool.{}_1'.format(ii)
+        )
 
-        total_stride = 1
-        network, total_stride = conv_layers(input_network, early_conv_dict,
-                                            total_stride,
-                                            init_input_size=input_size,
-                                            p_dropout=0,
-                                            base_name='early{}'.format(jj))
-        total_stride_list.append(total_stride)
-        conv_network_list.append(network)
+        network = cl.Conv2DXLayer(
+            network,
+            num_filters=32, filter_size=(8, 1),
+            stride=(1, 1),
+            nonlinearity=lasagne.nonlinearities.rectify,
+            name='early_conv.{}_2'.format(ii)
+        )
+        network = layers.MaxPool2DXLayer(
+            network,
+            pool_size=(4, 1),
+            stride=(4, 1),
+            ignore_border=False,
+            name='early_maxpool.{}_2'.format(ii)
+        )
 
-    # Concatenate
-    network = layers.ConcatLayer(conv_network_list,
+        earlyconv_list.append(network)
+
+    network = layers.ConcatLayer(earlyconv_list,
                                  axis=1,
                                  cropping=[None, None, 'lower', None],
-                                 name='MultisourceConcatenate')
+                                 name='multiscale_concat')
 
-    # late conv layers (dense layers)
-    network, total_stride = conv_layers(network, late_conv_dict,
-                                        total_stride,
-                                        init_input_size=1,
-                                        p_dropout=p_dropout,
-                                        base_name='late')
+    # late conv layers
+    network = layers.Conv2DLayer(
+        layers.dropout(network, p=0.5), 512, (1, 1), (1, 1),
+        name='late_conv.{}_1'.format(ii)
+    )
 
-    # frame output layer. every frame has a value
-    network = cl.Conv2DXLayer(
-        lasagne.layers.dropout(network, p=p_dropout),
-        num_filters=output_size, filter_size=(dense_filter_size, 1),
-        nonlinearity=lasagne.nonlinearities.sigmoid,
-        W=lasagne.init.GlorotUniform()
+    network = layers.Conv2DLayer(
+        layers.dropout(network, p=0.5), 512, (1, 1), (1, 1),
+        name='late_conv.{}_2'.format(ii)
+    )
+
+    network = layers.Conv2DLayer(
+        layers.dropout(network, p=0.5), 188, (1, 1), (1, 1),
+        name='late_conv.{}_output'.format(ii)
     )
 
     # pool
-    network = layers.GlobalPoolLayer(network,
-                                     pool_function=final_pool_function)
-    network = layers.ReshapeLayer(network, ([0], -1))
+    network = layers.GlobalPoolLayer(network, pool_function=T.mean)
+    # network = layers.ReshapeLayer(network, ([0], -1))
 
-    return network
+    return network, input_var_list
--- Scripts/train_model.py
+++ Scripts/train_model.py
@@ -1,7 +1,12 @@
+import numpy as np
 import lasagne
+from lasagne import layers
+import theano
 import theano.tensor as T
+import network_structure as ns
 from clip2frame import utils
 
+floatX = theano.config.floatX
 
 if __name__ == '__main__':
     # Data options
@@ -9,6 +14,7 @@ if __name__ == '__main__':
     param_fp = '../data/models/sample_model.npz'
 
     # Training options
+    build_func = ns.build_fcn_gaussian_multiscale
     lr = 0.01
     loss_function = lasagne.objectives.binary_crossentropy
     n_epochs = 10
@@ -20,35 +26,6 @@ if __name__ == '__main__':
     ]
     n_sources = len(scale_list)
 
-    # Network options
-    network_type = 'fcn_gaussian_multiscale'
-    n_early_conv = 2
-    early_pool_size = 4
-    network_options = {
-        'early_conv_dict_list': [
-            {'conv_filter_list': [(32, 8) for ii in range(n_early_conv)],
-             'pool_filter_list': [early_pool_size
-                                  for ii in range(n_early_conv)],
-             'pool_stride_list': [None for ii in range(n_early_conv)]}
-            for ii in range(n_sources)
-        ],
-        'late_conv_dict': {
-            'conv_filter_list': [(512, 1), (512, 1)],
-            'pool_filter_list': [None, None],
-            'pool_stride_list': [None, None]
-        },
-        'dense_filter_size': 1,
-        'scan_dict': {
-            'scan_filter_list': [256],
-            'scan_std_list': [256/early_pool_size**n_early_conv],
-            'scan_stride_list': [1],
-        },
-        'final_pool_function': T.mean,  # T.max
-        'input_size_list': [128 for nn in range(n_sources)],
-        'output_size': 188,
-        'p_dropout': 0.5
-    }
-
     # Loading data
     print("Loading data...")
     X_tr_list, y_tr, X_va_list, y_va, X_te_list, y_te = \
@@ -56,17 +33,38 @@ if __name__ == '__main__':
             data_dir, scale_list
         )
 
-    network, input_var_list, lr_var, train_func, val_func, pr_func = \
-        utils.make_network_multiscale(
-            network_type, loss_function, lr, n_sources,
-            network_options, make_pr_func=True
-        )
+    # Building Network
+    network, input_var_list = build_func(mean_std_list)
+
+    # Compute loss
+    target_var = T.matrix('targets')
+    lr_var = theano.shared(np.array(lr, dtype=floatX))
+    output_var = layers.get_output(network)
+    epsilon = np.float32(1e-6)
+    one = np.float32(1)
+    output_var = T.clip(output_var, epsilon, one-epsilon)
+    loss_var = loss_function(output_var, target_var)
+    loss_var = loss_var.mean()
+
+    params = layers.get_all_params(network, trainable=True)
+    updates = lasagne.updates.adagrad(
+        loss_var, params, learning_rate=lr_var)
+
+    output_va_var = layers.get_output(network, deterministic=True)
+    output_va_var = T.clip(output_va_var, epsilon, one-epsilon)
+    loss_va_var = loss_function(output_va_var, target_var)
+    loss_va_var = loss_va_var.mean()
+
+    func_tr = theano.function(
+        input_var_list+[target_var], loss_var, updates=updates)
+    func_va = theano.function(
+        input_var_list+[target_var], [output_va_var, loss_va_var])
 
     # Training
     utils.train_multiscale(
         X_tr_list, y_tr, X_va_list, y_va,
         network,
-        train_func, val_func,
+        func_tr, func_va,
         n_epochs, batch_size, lr_var,
         param_fp=param_fp
     )
